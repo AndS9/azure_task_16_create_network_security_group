ae961091490766aba479a8a3fe84c27a87d18fc7
Andrii Shukalo <shukaload@gmail.com>
2026-07-14 23:07:08 +0300

    Fixes to task.ps1

--- task.ps1
+++ task.ps1
@@ -14,13 +14,17 @@ $mngSubnetIpRange = "10.20.30.128/26"
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
+$common_rule =New-AzNetworkSecurityRuleConfig -Name AllowVnetInbound `
+    -Access Allow -Protocol * -Direction Inbound -Priority 101 -SourceAddressPrefix `
+    VirtualNetwork -SourcePortRange * -DestinationAddressPrefix VirtualNetwork -DestinationPortRange *
+
 Write-Host "Creating web network security group..."
 $rule = New-AzNetworkSecurityRuleConfig -Name AllowHttpHttps `
     -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
     Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange @(80,443)
 
 $nsgWeb = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-    $webSubnetName -SecurityRules $rule
+    $webSubnetName -SecurityRules $common_rule, $rule
 
 Write-Host "Creating mngSubnet network security group..."
 $rule = New-AzNetworkSecurityRuleConfig -Name ssh-rule -Description "Allow SSH" `
@@ -28,19 +32,19 @@ $rule = New-AzNetworkSecurityRuleConfig -Name ssh-rule -Description "Allow SSH" 
     Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
 
 $nsgMng = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-    $mngSubnetName -SecurityRules $rule
+    $mngSubnetName -SecurityRules $common_rule, $rule
 Write-Host "Creating dbSubnet network security group..."
 
 #I comment code that create DenyAllOutbound rule because this rule block tests
 #And this rule is NSG default rule at all
-<#
+
 $rule = New-AzNetworkSecurityRuleConfig -Name DenyAllOutbound -Description "DenyAllOutbound" `
     -Access Deny -Protocol * -Direction Inbound -Priority 100 -SourceAddressPrefix `
     Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange *
-#>
+
 
 $nsgDb = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-    $dbSubnetName #-SecurityRules $rule
+    $dbSubnetName -SecurityRules $common_rule, $rule
 
 Write-Host "Creating a virtual network ..."
 $webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $nsgWeb
